413cb087d7cbd42b6ba845e28a3db28e408b265b
sumedh1710 <64302716+sumedh1710@users.noreply.github.com>
2023-12-03 21:36:28 -0500

    Update pythonfp1.py

--- pythonfp1.py
+++ pythonfp1.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-spotify_data = pd.read_csv('.\sampled_dataset.csv')
+spotify_data = pd.read_csv('sampled_dataset.csv')
 
 
 
